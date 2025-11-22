479df7fa2728d61330625702abe9452082918131
Vince373 <vpark1@terpmail.umd.edu>
2025-11-22 17:30:37 -0500

    Add files via upload

---
+++ Histogram Distribution of Airbnb Density Across ZIP Codes.R
@@ -0,0 +1,8 @@
+hist(
+  dataset$airbnb_count,
+  main = "Distribution of Airbnb Density Across ZIP Codes",
+  xlab = "Airbnb Count (Listings per ZIP Code)",
+  ylab = "Number of ZIP Codes",
+  col = "lightblue",
+  border = "black"
+)
