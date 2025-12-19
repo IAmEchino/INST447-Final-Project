16c29bea39c99808243c2e4efbad82877aaf1d82
jinnjenga <122967001+jinnjenga@users.noreply.github.com>
2025-12-19 18:07:28 -0500

    Delete linearReg_Q3.R

--- linearReg_Q3.R
+++
@@ -1,7 +0,0 @@
-lin_reg = lm(zillow_count~airbnb_count, data = merged_csv)
-summary(lin_reg)
-lin_reg
-
-#Code from geeksforgeeks.org
-ggplot() + geom_point(aes(x = merged_csv$airbnb_count, y = merged_csv$zillow_count)) +
-  geom_line(aes(x =  merged_csv$airbnb_count, y = predict(lin_reg, newdata = merged_csv)))
